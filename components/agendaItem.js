8586633588f631f736cb012803073d5591d08dca
Brett McMillin <brettmcm@me.com>
2023-05-12 09:53:33 -0700

    Refining slider control

--- components/agendaItem.js
+++ components/agendaItem.js
@@ -6,9 +6,11 @@ import { DragHandle } from './icons';
 
 export default function AgendaItem(props) {
 
+
   const [timeAdjust, timeAdjustActive] = React.useState([false]);
 
   const [setCurrentBarValue, changeValue] = React.useState([0]);
+  const [definedTime, redefineTime] = React.useState(props.time);
 
   const handleMouseDown = useCallback((e) => {
     timeAdjustActive(false)
@@ -16,19 +18,32 @@ export default function AgendaItem(props) {
     const container = e.target.parentNode;
 
     const barTotal = container.clientWidth;
+    changeValue(Math.round((barTotal/5) * (props.time/5)));
 
-    document.addEventListener('mousemove', function(event) { 
+    var sliderMove = function(event) {
+
+      const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+
       var rect = container.getBoundingClientRect(); 
-      var x = event.clientX - rect.left; 
+      var x = event.clientX - rect.left;
+      const initialX = 25;
+      var initialValue = Math.round((barTotal/5) * (definedTime/5));
+      var differential = initialValue - initialX;
+      var targetValue = clamp(x + differential, 0, barTotal);
       
-      changeValue(Math.round(barTotal * (x / barTotal)));
-    }); 
+      changeValue(targetValue);
+      redefineTime(targetValue);
+    };
+
+    document.addEventListener('mousemove', sliderMove); 
 
     document.addEventListener(
       "mouseup",
       () => {
         timeAdjustActive(true)
         document.body.style.cursor = "default";
+        changeValue(0);
+        document.removeEventListener('mousemove', sliderMove); 
       },
       { once: true }
     );
@@ -38,7 +53,7 @@ export default function AgendaItem(props) {
       <div className={timeAdjust ? styles.agendaItem : styles.agendaItemAdjusting}>
         <div className={styles.time}
           onMouseDown={handleMouseDown}
-          >{props.time}m</div>
+          >{definedTime}m</div>
         <div className={styles.itemName} contentEditable="true" suppressContentEditableWarning="true">{props.title}</div>
         <DragHandle />
         <div className={timeAdjust ? styles.timeBarHidden : styles.timeBar} style={{width: setCurrentBarValue}}></div>
